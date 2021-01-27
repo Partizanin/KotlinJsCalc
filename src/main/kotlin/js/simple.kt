56a3be756b9ecc620657bbe9605f8e2ff6aaf7b3
Partizanin <topiciv1@yandex.ua>
2021-01-27 17:58:50 +0200

    clean up,improve code add gradle folder

--- src/main/kotlin/js/simple.kt
+++ src/main/kotlin/js/simple.kt
@@ -8,26 +8,36 @@ import org.w3c.dom.get
 
 
 fun main() {
-    console.log("Hello, ${greet()}")
-
     window.addEventListener("load", {
         console.log("page loaded")
         init()
     })
 }
 
+fun init() {
+    val elementsByTagName = document.getElementsByTagName("input")
+    val resultElement = getInputElement()
+    resultElement.defaultValue = ""
+    for (i in 0 until elementsByTagName.length) {
+        val element = elementsByTagName[i] as HTMLInputElement
+        if (element.type == "button") {
+            element.addEventListener("click", { buttonListener(it) })
+        }
+    }
+}
+
+fun getInputElement() = document.getElementById("result") as HTMLInputElement
+
 fun buttonListener(event: Event) {
     val eventTarget = event.target as HTMLInputElement
-
     showInput(eventTarget.value)
 }
 
 fun showInput(value: String) {
-    val resultElement = document.getElementById("result") as HTMLInputElement
-    val list = resultElement.defaultValue.toCharArray().toList().map { it.toString() }
-    if (list.contains("=")) {
-        resultElement.defaultValue = ""
-    }
+    val resultElement = getInputElement()
+    val listOfInputChars = getListOfInputStrings(resultElement)
+    if (listOfInputChars.contains("=")) resultElement.defaultValue = ""
+
     when (value) {
         "+" -> addOperation(value)
         "-" -> addOperation(value)
@@ -39,34 +49,26 @@ fun showInput(value: String) {
     }
 }
 
-fun addOperation(value: String) {
-    val resultElement = document.getElementById("result") as HTMLInputElement
-    val operationList = listOf("+", "-", "*", "/", "C")
-    val listOfTextStrings = resultElement.defaultValue.toMutableList().map { it.toString() }
+fun getListOfInputStrings(resultElement: HTMLInputElement): List<String> {
+    return resultElement.defaultValue.toCharArray().toList().map { it.toString() }
+}
 
-    if (!operationList.any { listOfTextStrings.contains(it) }&&listOfTextStrings.isNotEmpty()) {
+
+fun addOperation(value: String) {
+    val resultElement = getInputElement()
+    val operationList = listOf("+", "-", "*", "/", "C")
+    val listOfTextStrings = getListOfInputStrings(resultElement)
+
+    if (!operationList.any { listOfTextStrings.contains(it) } && listOfTextStrings.isNotEmpty()) {
         resultElement.defaultValue = resultElement.defaultValue + value
     }
 }
 
 fun countExpression(inputTextValue: String) {
     if (inputTextValue.isNotEmpty()) {
-        val resultElement = document.getElementById("result") as HTMLInputElement
+        val resultElement = getInputElement()
         val result = eval(inputTextValue).toString()
         resultElement.defaultValue = "$inputTextValue=$result"
-    }
-}
-
-fun init() {
-    val elementsByTagName = document.getElementsByTagName("input")
-    val resultElement = document.getElementById("result") as HTMLInputElement
-    resultElement.defaultValue = ""
-    for (i in 0 until elementsByTagName.length) {
-        val element1 = elementsByTagName[i]
-        val element = element1 as HTMLInputElement
-        if (element.type == "button") {
-            element.addEventListener("click", { buttonListener(it) })
-        }
     }
 }
 
